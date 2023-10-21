2e8d50a44b9e1980572ebfd92df7d32542f0a3b0
sariunar <rinchinovasariuna@gmail.com>
2023-10-21 21:48:54 +0800

    add footer

--- src/widgets/footer/footer.jsx
+++ src/widgets/footer/footer.jsx
@@ -3,5 +3,7 @@ import React from "react";
 import "./footer.css";
 
 export const Footer = () => {
-    return <div className="footer">Footer</div>
+    return <div className="footer">
+        <p>2021 All Rights Reserved.Ojjomedia</p>
+    </div>
 }
