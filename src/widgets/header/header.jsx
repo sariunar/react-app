dcf10b58e04c09b1af4b18836bf4cd7bd00a05df
sariunar <rinchinovasariuna@gmail.com>
2023-10-21 21:46:17 +0800

    add header

--- src/widgets/header/header.jsx
+++ src/widgets/header/header.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Element } from "react-scroll";
+import photo from "./images/photo.png";
 
 import "./header.css";
 
 export const Header = () => {
-    return <Element name="home" className="header">Header</Element>
+    return <Element name="home" className="header">
+        <div>
+            <h2 className="header__title">I’m Rayan Adlrdard</h2>
+            <h2 className="header__title"><p>Front-end </p> Developer </h2>
+            <p className="header__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
+            <button className="header__button">HIRE ME</button>
+        </div>
+        <img src={photo} alt="photo" />
+    </Element>
 }
