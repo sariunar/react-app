c95523c70ec32b43e8ae362131e740eb7db61b0e
sariunar <rinchinovasariuna@gmail.com>
2023-10-21 21:14:29 +0800

    add portfolio block

---
+++ src/constants/portfolio.js
@@ -0,0 +1,53 @@
+import ImgOne from "./images/Image.png";
+import ImgTwo from "./images/Image2.png";
+import ImgThree from "./images/Image3.png";
+import ImgFour from "./images/Image4.png";
+import ImgFive from "./images/Image5.png";
+import ImgSix from "./images/Image6.png";
+import ImgSeven from "./images/Image7.png";
+import ImgEight from "./images/Image8.png";
+import ImgNine from "./images/Image9.png";
+
+export const PORTFOLIO_TITLE = {
+    header: "Portfolio",
+    title:"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum"
+}
+export const CATEGORIES = ['All categories', 'UI Design', 'Web Templates', 'Logo'];
+export const PORTFOLIO_DATA = [{
+    img: ImgNine,
+    link: '/',
+    category: CATEGORIES[1],
+},{
+    img: ImgOne,
+    link: '/',
+    category: CATEGORIES[1],
+},{
+    img: ImgTwo,
+    link: '/',
+    category: CATEGORIES[1],
+},{
+    img: ImgThree,
+    link: '/',
+    category: CATEGORIES[2],
+},{
+    img: ImgFour,
+    link: '/',
+    category: CATEGORIES[2],
+},{
+    img: ImgFive,
+    link: '/',
+    category: CATEGORIES[2],
+},{
+    img: ImgSix,
+    link: '/',
+    category: CATEGORIES[3],
+},{
+    img: ImgSeven,
+    link: '/',
+    category: CATEGORIES[3],
+},{
+    img: ImgEight,
+    link: '/',
+    category: CATEGORIES[3],
+},
+]
--- src/widgets/portfolio/portfolio.jsx
+++ src/widgets/portfolio/portfolio.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Element } from "react-scroll";
+import { CATEGORIES, PORTFOLIO_DATA } from "../../constants/portfolio";
+import { PortfolioTabs } from "../../features/portfolioTabs/portfolioTabs";
+import { PortfolioCard } from "../../shared/portfolioCard/portfolioCard";
+import { PORTFOLIO_TITLE } from "../../constants/portfolio";
+import { Header } from "../../shared/header/header";
+import { Paragraph } from "../../shared/paragraph/paragraph";
 
 import "./portfolio.css";
 
 export const Portfolio = () => {
-    return <Element name="portfolio" className="portfolio">Portfolio</Element>
+    const [portfolioData, setPortfolioData] = useState(PORTFOLIO_DATA);
+    const rebderCards = (data) => {
+        return data.map((item, index) => <PortfolioCard data={item} key={`${index}_card`} />)
+    }
+    const handleTabClick = (category) => {
+        if (category === CATEGORIES[0]) {
+            setPortfolioData(PORTFOLIO_DATA);
+        } else {
+            const newData = PORTFOLIO_DATA.filter(item => item.category === category);
+            setPortfolioData(newData);
+        }
+    }
+
+    return <Element name="portfolio" className="portfolio">
+        <div className="headerInfo">
+            <Header header={PORTFOLIO_TITLE.header} />
+            <Paragraph aling="center" text={PORTFOLIO_TITLE.title} />
+        </div>
+        <PortfolioTabs onClick={handleTabClick} />
+        <div className="portfolioWrapper">
+            {rebderCards(portfolioData)}
+        </div>
+    </Element>
 }
